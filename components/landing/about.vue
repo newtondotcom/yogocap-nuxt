3dd29490dcbd98934589b8882ba8798fd7f43c61
newtondotcom <asphalt9fr@gmail.com>
2024-04-09 22:58:43 +0200

    component translation from svelte

---
+++ components/landing/about.vue
@@ -0,0 +1,35 @@
+<template>
+<section id="about" class="w-full h-full">
+    <div
+      class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
+    >
+      <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+        <div
+          class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full"
+        >
+          <img
+            alt="Party"
+            src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            class="absolute inset-0 h-full w-full object-cover"
+          />
+        </div>
+  
+        <div class="lg:py-24">
+          <h2 class="text-3xl font-bold text-primary-600 sm:text-4xl">Yogocap : High-Quality AI Subtitles at Unbeatable Prices 🌟</h2>
+  
+          <p class="mt-4 text-gray-600">
+            Discover Yogocap, the most cost-effective AI-powered solution for subtitling your social media videos.
+             In a digital world where clear communication is vital, Yogocap offers an affordable way to ensure your message reaches everyone.
+          </p>
+  
+          <button
+            
+            class="mt-8 inline-block rounded bg-primary-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-primary-700 focus:outline-none focus:ring focus:ring-yellow-400"
+          >
+            Get Started Today
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
