4ee44b8b6f508d5c5c14b008344dbff8ba05bcc1
newtondotcom <asphalt9fr@gmail.com>
2024-04-09 22:02:16 +0200

    seo + layout

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Yogocap',
+  ogTitle: 'Yogocap',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://img.freepik.com/vecteurs-libre/vecteur-degrade-logo-colore-oiseau_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1712534400&semt=sph',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
