664b293f380adb641ae34b3ce0dff5bebc69242f
newtondotcom <asphalt9fr@gmail.com>
2024-04-10 00:17:23 +0200

    getting near page restyle completed

--- pages/nvideo.vue
+++ pages/nvideo.vue
@@ -8,6 +8,7 @@ let videoMusic = ref(false);
 let videoSent = ref(false);
 let videoEmoji = ref(true);
 let videoValid = ref(false);
+let Success = ref(false);
 let durationInSeconds = ref(0);
 let length = ref(25);
 
@@ -55,7 +56,9 @@ async function launchVideoProcessing() {
         emoji: true,
         silence: true,
     };
-    await sendAMQP(task);
+    //await sendAMQP(task);
+    length.value = 100;
+    Success.value = true;
 }
 
 async function handleFileChange(event: { target: any; }) {
@@ -194,40 +197,19 @@ onMounted(() => {
                         <polyline fill="none" stroke-miterlimit="10" points="15,5 22,12 15,19 "></polyline>
                     </svg>
                 </div>
-                <div class="text-gray-600 flex flex-row">
-                    Do you want us to cut silent parts of the video ?
-                    <div class="mt-2"><input :value="videoCut"
-                            class="w-5 h-5 ease-soft text-base mr-2 rounded-1.4 checked:bg-gradient-to-tl checked:from-gray-900 checked:to-slate-800 after:text-xxs after:font-awesome after:duration-250 after:ease-soft-in-out duration-250 relative float-left mt-1 cursor-pointer appearance-none border border-solid border-slate-150 bg-white bg-contain bg-center bg-no-repeat align-top transition-all after:absolute after:flex after:h-full after:w-full after:items-center after:justify-center after:text-white after:opacity-0 after:transition-all after:content-['\2713'] checked:border-0 checked:border-transparent checked:bg-transparent checked:after:opacity-100"
-                            type="checkbox" /></div>
-                </div>
-                <div v-if="!datas.canMusic" class="text-gray-600 opacity-50 flex flex-row">
-                    Do you want us to add a dynamic music to the video ?
-                    <div class="mt-2"><input disabled v-model="videoMusic"
-                            class="w-5 h-5 ease-soft text-base mr-2 rounded-1.4 checked:bg-gradient-to-tl checked:from-gray-900 checked:to-slate-800 after:text-xxs after:font-awesome after:duration-250 after:ease-soft-in-out duration-250 relative float-left mt-1 cursor-pointer appearance-none border border-solid border-slate-150 bg-white bg-contain bg-center bg-no-repeat align-top transition-all after:absolute after:flex after:h-full after:w-full after:items-center after:justify-center after:text-white after:opacity-0 after:transition-all after:content-['\2713'] checked:border-0 checked:border-transparent checked:bg-transparent checked:after:opacity-100"
-                            type="checkbox" /></div>
-                </div>
-                <div v-else class="text-gray-600 flex flex-row">
-                    Do you want us to add a dynamic music to the video ?
-                    <div class="mt-2"><input v-model="videoMusic"
-                            class="w-5 h-5 ease-soft text-base mr-2 rounded-1.4 checked:bg-gradient-to-tl checked:from-gray-900 checked:to-slate-800 after:text-xxs after:font-awesome after:duration-250 after:ease-soft-in-out duration-250 relative float-left mt-1 cursor-pointer appearance-none border border-solid border-slate-150 bg-white bg-contain bg-center bg-no-repeat align-top transition-all after:absolute after:flex after:h-full after:w-full after:items-center after:justify-center after:text-white after:opacity-0 after:transition-all after:content-['\2713'] checked:border-0 checked:border-transparent checked:bg-transparent checked:after:opacity-100"
-                            type="checkbox" /></div>
-                </div>
+                <UCheckbox class="flex" v-model="videoCut" name="notifications" label="Do you want us to cut silent parts of the video ?" />
+                <UCheckbox v-if="datas.canMusic" class="flex" v-model="videoMusic" name="notifications" label="Do you want us to add a dynamic music to the video ?" />
+                <UCheckbox v-else class="flex" v-model="videoMusic" name="notifications" label="Do you want us to add a dynamic music to the video ?" />
 
-                <div v-if="!datas.canEmoji" class="text-gray-600 opacity-50 flex flex-row">
-                    Do you want us to add emojis to the video ?
-                    <div class="mt-2"><input disabled v-model="videoEmoji"
-                            class="w-5 h-5 ease-soft text-base mr-2 rounded-1.4 checked:bg-gradient-to-tl checked:from-gray-900 checked:to-slate-800 after:text-xxs after:font-awesome after:duration-250 after:ease-soft-in-out duration-250 relative float-left mt-1 cursor-pointer appearance-none border border-solid border-slate-150 bg-white bg-contain bg-center bg-no-repeat align-top transition-all after:absolute after:flex after:h-full after:w-full after:items-center after:justify-center after:text-white after:opacity-0 after:transition-all after:content-['\2713'] checked:border-0 checked:border-transparent checked:bg-transparent checked:after:opacity-100"
-                            type="checkbox" /></div>
-                </div>
-                <div v-else class="text-gray-600 flex flex-row">
-                    Do you want us to add emojis music to the video ?
-                    <div class="mt-2"><input v-model="videoEmoji" id="checkbox-1"
-                            class="w-5 h-5 ease-soft text-base mr-2 rounded-1.4 checked:bg-gradient-to-tl checked:from-gray-900 checked:to-slate-800 after:text-xxs after:font-awesome after:duration-250 after:ease-soft-in-out duration-250 relative float-left mt-1 cursor-pointer appearance-none border border-solid border-slate-150 bg-white bg-contain bg-center bg-no-repeat align-top transition-all after:absolute after:flex after:h-full after:w-full after:items-center after:justify-center after:text-white after:opacity-0 after:transition-all after:content-['\2713'] checked:border-0 checked:border-transparent checked:bg-transparent checked:after:opacity-100"
-                            type="checkbox" /></div>
-                </div>
+                <UCheckbox v-if="datas.canEmoji" class="flex" v-model="videoEmoji" name="notifications" label="Do you want us to add emojis to the video ?" />
+                <UCheckbox v-else class="flex" v-model="videoEmoji" name="notifications" label="Do you want us to add emojis to the video ?" />
 
+                <div class="mt-6 justify-end">
+                    <UButton @click="length = 75">Next</UButton>
+                </div>
             </div>
-            <div :class="[videoUploaded ? 'opacity-1' : 'opacity-20' ]" v-if="length==75" >
+
+            <div :class="[!videoUploaded ? 'opacity-1' : 'opacity-20' ]" v-if="length==75" >
                 <div class="flex items-center justify-between mb-6">
                     <p class="text-2xl font-bold">Step 3</p>
                     <svg class="w-6 text-gray-700 transform rotate-90 sm:rotate-0" stroke="currentColor"
@@ -238,25 +220,16 @@ onMounted(() => {
                 </div>
                 <div v-if="videoValid">
                     <span class="text-gray-600">
-                        Your video is <span class="text-primary-500">{durationInSeconds} seconds</span>
-                        long. Name it :
+                        FYI, your video is <span class="text-primary-500">{{durationInSeconds}} seconds</span>
+                        long. <br> Name it :
                     </span>
-                    <div class="relative">
-                        <input type="email" placeholder="video"
-                            class="w-full rounded-md border-gray-200 pe-10 ps-2 shadow-sm sm:text-sm" />
-                        <span
-                            class="pointer-events-none absolute inset-y-0 end-0 grid w-10 place-content-center text-gray-500">
-                            <svg class="w-4" viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                                <g id="SVGRepo_iconCarrier">
-                                    <path
-                                        d="M13.2942 7.95881C13.5533 7.63559 13.5013 7.16358 13.178 6.90453C12.8548 6.64549 12.3828 6.6975 12.1238 7.02072L13.2942 7.95881ZM6.811 14.8488L7.37903 15.3385C7.38489 15.3317 7.39062 15.3248 7.39623 15.3178L6.811 14.8488ZM6.64 15.2668L5.89146 15.2179L5.8908 15.2321L6.64 15.2668ZM6.5 18.2898L5.7508 18.2551C5.74908 18.2923 5.75013 18.3296 5.75396 18.3667L6.5 18.2898ZM7.287 18.9768L7.31152 19.7264C7.36154 19.7247 7.41126 19.7181 7.45996 19.7065L7.287 18.9768ZM10.287 18.2658L10.46 18.9956L10.4716 18.9927L10.287 18.2658ZM10.672 18.0218L11.2506 18.4991L11.2571 18.491L10.672 18.0218ZM17.2971 10.959C17.5562 10.6358 17.5043 10.1638 17.1812 9.90466C16.8581 9.64552 16.386 9.69742 16.1269 10.0206L17.2971 10.959ZM12.1269 7.02052C11.8678 7.34365 11.9196 7.81568 12.2428 8.07484C12.5659 8.33399 13.0379 8.28213 13.2971 7.95901L12.1269 7.02052ZM14.3 5.50976L14.8851 5.97901C14.8949 5.96672 14.9044 5.95412 14.9135 5.94123L14.3 5.50976ZM15.929 5.18976L16.4088 4.61332C16.3849 4.59344 16.3598 4.57507 16.3337 4.5583L15.929 5.18976ZM18.166 7.05176L18.6968 6.52192C18.6805 6.50561 18.6635 6.49007 18.6458 6.47532L18.166 7.05176ZM18.5029 7.87264L19.2529 7.87676V7.87676L18.5029 7.87264ZM18.157 8.68976L17.632 8.15412C17.6108 8.17496 17.5908 8.19704 17.5721 8.22025L18.157 8.68976ZM16.1271 10.0203C15.8678 10.3433 15.9195 10.8153 16.2425 11.0746C16.5655 11.3339 17.0376 11.2823 17.2969 10.9593L16.1271 10.0203ZM13.4537 7.37862C13.3923 6.96898 13.0105 6.68666 12.6009 6.74805C12.1912 6.80943 11.9089 7.19127 11.9703 7.60091L13.4537 7.37862ZM16.813 11.2329C17.2234 11.1772 17.5109 10.7992 17.4552 10.3888C17.3994 9.97834 17.0215 9.69082 16.611 9.74659L16.813 11.2329ZM12.1238 7.02072L6.22577 14.3797L7.39623 15.3178L13.2942 7.95881L12.1238 7.02072ZM6.24297 14.359C6.03561 14.5995 5.91226 14.9011 5.89159 15.218L7.38841 15.3156C7.38786 15.324 7.38457 15.3321 7.37903 15.3385L6.24297 14.359ZM5.8908 15.2321L5.7508 18.2551L7.2492 18.3245L7.3892 15.3015L5.8908 15.2321ZM5.75396 18.3667C5.83563 19.1586 6.51588 19.7524 7.31152 19.7264L7.26248 18.2272C7.25928 18.2273 7.25771 18.2268 7.25669 18.2264C7.25526 18.2259 7.25337 18.2249 7.25144 18.2232C7.2495 18.2215 7.24825 18.2198 7.24754 18.2185C7.24703 18.2175 7.24637 18.216 7.24604 18.2128L5.75396 18.3667ZM7.45996 19.7065L10.46 18.9955L10.114 17.536L7.11404 18.247L7.45996 19.7065ZM10.4716 18.9927C10.7771 18.9151 11.05 18.7422 11.2506 18.499L10.0934 17.5445C10.0958 17.5417 10.0989 17.5397 10.1024 17.5388L10.4716 18.9927ZM11.2571 18.491L17.2971 10.959L16.1269 10.0206L10.0869 17.5526L11.2571 18.491ZM13.2971 7.95901L14.8851 5.97901L13.7149 5.04052L12.1269 7.02052L13.2971 7.95901ZM14.9135 5.94123C15.0521 5.74411 15.3214 5.6912 15.5243 5.82123L16.3337 4.5583C15.4544 3.99484 14.2873 4.2241 13.6865 5.0783L14.9135 5.94123ZM15.4492 5.7662L17.6862 7.6282L18.6458 6.47532L16.4088 4.61332L15.4492 5.7662ZM17.6352 7.58161C17.7111 7.6577 17.7535 7.761 17.7529 7.86852L19.2529 7.87676C19.2557 7.36905 19.0555 6.88127 18.6968 6.52192L17.6352 7.58161ZM17.7529 7.86852C17.7524 7.97604 17.7088 8.07886 17.632 8.15412L18.682 9.22541C19.0446 8.87002 19.2501 8.38447 19.2529 7.87676L17.7529 7.86852ZM17.5721 8.22025L16.1271 10.0203L17.2969 10.9593L18.7419 9.15928L17.5721 8.22025ZM11.9703 7.60091C12.3196 9.93221 14.4771 11.5503 16.813 11.2329L16.611 9.74659C15.0881 9.95352 13.6815 8.89855 13.4537 7.37862L11.9703 7.60091Z"
-                                        fill="currentColor"></path>
-                                </g>
-                            </svg>
-                        </span>
-                    </div>
+                <UInput
+                    icon="i-heroicons-pencil-16-solid"
+                    size="sm"
+                    color="white"
+                    :trailing="false"
+                    placeholder="a simple name"
+                />
                 </div>
                 <div v-else class="text-gray-600">
                     Your video length is above the one included in your current plan.
@@ -265,7 +238,7 @@ onMounted(() => {
                 <div v-if="!loadingUpload && !videoSent" class="mt-4 ml-28">
                     <button
                         class="inline-flex items-center gap-2 rounded border border-primary-600 bg-primary-600 px-4 py-2 text-white hover:bg-transparent hover:text-primary-600 focus:outline-none focus:ring active:text-primary-500"
-                        on:click={launchAmpq}>
+                        @click="launchAmpq">
                         <span class="text-sm font-medium"> Process it </span>
 
                         <svg class="h-5 w-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -275,24 +248,6 @@ onMounted(() => {
                         </svg>
                     </button>
                 </div>
-            </div>
-            <div v-if="length==100" class="opacity-20">
-                <div class="flex items-center justify-between mb-6">
-                    <p class="text-2xl font-bold">Progressing</p>
-                    <svg class="w-8 h-8 text-gray-600" viewBox="0 0 16 16" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                        <g id="SVGRepo_iconCarrier">
-                            <path
-                                d="M7.706 0.290 C 7.484 0.362,7.356 0.490,7.294 0.699 C 7.259 0.816,7.253 1.088,7.253 2.508 C 7.253 4.389,7.251 4.365,7.443 4.557 C 7.700 4.813,8.300 4.813,8.557 4.557 C 8.749 4.365,8.747 4.389,8.747 2.508 C 8.747 0.688,8.744 0.656,8.596 0.480 C 8.472 0.333,8.339 0.284,8.040 0.276 C 7.893 0.272,7.743 0.278,7.706 0.290 M2.753 2.266 C 2.595 2.338,2.362 2.566,2.281 2.728 C 2.197 2.897,2.193 3.085,2.269 3.253 C 2.343 3.418,4.667 5.750,4.850 5.843 C 5.109 5.976,5.375 5.911,5.643 5.649 C 5.907 5.391,5.977 5.111,5.843 4.850 C 5.750 4.667,3.418 2.343,3.253 2.269 C 3.101 2.200,2.901 2.199,2.753 2.266 M12.853 2.282 C 12.730 2.339,12.520 2.536,11.518 3.541 C 10.597 4.464,10.316 4.762,10.271 4.860 C 10.195 5.025,10.196 5.216,10.272 5.378 C 10.342 5.528,10.572 5.764,10.727 5.845 C 10.884 5.927,11.117 5.926,11.280 5.843 C 11.447 5.757,13.757 3.447,13.843 3.280 C 13.926 3.118,13.927 2.884,13.846 2.729 C 13.764 2.572,13.552 2.364,13.392 2.283 C 13.213 2.192,13.048 2.192,12.853 2.282 M0.699 7.292 C 0.404 7.385,0.258 7.620,0.258 7.999 C 0.259 8.386,0.403 8.618,0.698 8.706 C 0.816 8.741,1.079 8.747,2.508 8.747 C 3.997 8.747,4.196 8.742,4.318 8.702 C 4.498 8.644,4.644 8.498,4.702 8.318 C 4.788 8.053,4.745 7.677,4.608 7.491 C 4.578 7.451,4.492 7.384,4.417 7.343 L 4.280 7.267 2.547 7.261 C 1.152 7.257,0.791 7.263,0.699 7.292 M11.745 7.278 C 11.622 7.308,11.452 7.411,11.392 7.492 C 11.255 7.677,11.212 8.053,11.298 8.318 C 11.356 8.498,11.502 8.644,11.682 8.702 C 11.804 8.742,12.003 8.747,13.492 8.747 C 14.921 8.747,15.184 8.741,15.302 8.706 C 15.597 8.618,15.741 8.386,15.742 7.999 C 15.742 7.614,15.595 7.383,15.290 7.291 C 15.187 7.260,14.864 7.254,13.496 7.256 C 12.578 7.258,11.790 7.268,11.745 7.278 M4.853 10.282 C 4.730 10.339,4.520 10.536,3.518 11.541 C 2.597 12.464,2.316 12.762,2.271 12.860 C 2.195 13.025,2.196 13.216,2.272 13.378 C 2.342 13.528,2.572 13.764,2.727 13.845 C 2.884 13.927,3.117 13.926,3.280 13.843 C 3.447 13.757,5.757 11.447,5.843 11.280 C 5.926 11.118,5.927 10.884,5.846 10.729 C 5.764 10.572,5.552 10.364,5.392 10.283 C 5.213 10.192,5.048 10.192,4.853 10.282 M10.753 10.266 C 10.595 10.338,10.362 10.566,10.281 10.728 C 10.197 10.897,10.193 11.085,10.269 11.253 C 10.343 11.418,12.667 13.750,12.850 13.843 C 13.109 13.976,13.375 13.911,13.643 13.649 C 13.907 13.391,13.977 13.111,13.843 12.850 C 13.750 12.667,11.418 10.343,11.253 10.269 C 11.101 10.200,10.901 10.199,10.753 10.266 M7.745 11.277 C 7.620 11.309,7.451 11.412,7.392 11.492 C 7.254 11.678,7.253 11.691,7.253 13.489 C 7.253 14.921,7.259 15.184,7.294 15.302 C 7.382 15.597,7.615 15.741,8.000 15.741 C 8.385 15.741,8.618 15.597,8.706 15.302 C 8.768 15.090,8.767 11.875,8.704 11.690 C 8.644 11.514,8.575 11.430,8.420 11.346 C 8.310 11.286,8.246 11.271,8.057 11.264 C 7.930 11.259,7.790 11.265,7.745 11.277 "
-                                stroke="none" fill-rule="evenodd" fill="currentColor"></path>
-                        </g>
-                    </svg>
-                </div>
-                <p class="text-gray-600">
-                    Complete all the previous steps to unlock this one !
-                </p>
             </div>
             <div v-if="length==100 && Success">
                 <div class="flex items-center justify-between mb-6">
