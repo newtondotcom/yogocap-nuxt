8273c82b33b13f9c65bb721d38c80f11207ab2da
newtondotcom <asphalt9fr@gmail.com>
2024-04-09 23:00:25 +0200

    page translation from svelte

---
+++ pages/dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="mt-8 ml-10">
+    <h1 class="text-2xl font-bold text-gray-900 sm:text-4xl ml-12 mt-24">
+      Welcome to <span class="">Yogocap</span>
+    </h1>
+
+
+    <p class="mt-1.5 text-l text-gray-600">
+      Discover the power of video captioning powered by AI to capture your audience attention. 🚀
+    </p>
+
+    <NuxtLink to="/nvideo"
+    class="absolute top-2/3 left-3/4 rounded bg-indigo-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-indigo-500"
+    >
+    Try with a video
+  </NuxtLink>
+  </div>
+</template>
