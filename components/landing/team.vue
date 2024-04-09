271235a4e3fbcd7936ad365a77de93d0a2cb636d
newtondotcom <asphalt9fr@gmail.com>
2024-04-09 22:59:42 +0200

    component translation from svelte

---
+++ components/landing/team.vue
@@ -0,0 +1,23 @@
+<template>
+<section id="team" class="bg-whites h-3/5">
+    <div class="container px-6 py-8 mx-auto">
+        <h2 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl">Our Team</h2>
+        <div class="flex flex-row text-center justify-center mt-4">
+            <div class="w-full max-w-xs text-center mx-10">
+                <img class="object-cover object-center w-full h-48 mx-auto rounded-lg" src="https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=739&q=80" alt="avatar" />
+                <div class="mt-2">
+                    <h3 class="text-lg font-medium text-gray-700">Louis Defrenne</h3>
+                    <span class="mt-1 font-medium text-gray-600 ">CEO</span>
+                </div>
+            </div>
+            <div class="w-full max-w-xs text-center mx-10">
+                <img class="object-cover object-center w-full h-48 mx-auto rounded-lg" src="https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=739&q=80" alt="avatar" />
+                <div class="mt-2">
+                    <h3 class="text-lg font-medium text-gray-700">Robin Augereau</h3>
+                    <span class="mt-1 font-medium text-gray-600 ">CTO</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</template>
