464942606fd9960a707861bf689631f517628873
newtondotcom <asphalt9fr@gmail.com>
2024-04-09 22:03:15 +0200

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="flex flex-col min-h-screen bg-slate-100">
+    <aside class="absolute top-0 left-0 flex w-64 flex-col h-full px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l"> 
+        <NuxtLink to="/dashboard">
+            <img class="w-auto h-6" src="https://merakiui.com/images/logo.svg" alt="">
+        </NuxtLink>
+    
+        <div class="flex flex-col justify-between flex-1 mt-8">
+            <nav class="-mx-3 space-y-6 ">
+                <div class="space-y-3 ">
+                    <label class="px-3 text-xs text-gray-500 uppercase">content</label>
+    
+                    <NuxtLink class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100  hover:text-gray-700 w-full" to="/nvideo">
+                        <svg viewBox="0 0 24 24" class="w-5 h-5" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M12 4V20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                        <span class="mx-2 text-sm font-medium">New Video</span>
+                    </NuxtLink>
+    
+                    <NuxtLink class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100  hover:text-gray-700 w-full" to="/previous">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
+                        </svg>
+    
+                        <span class="mx-2 text-sm font-medium">Previous videos</span>
+                    </NuxtLink>
+                </div>
+    
+                <div class="space-y-3 ">
+                    <label class="px-3 text-xs text-gray-500 uppercase">subscription</label>  
+    
+                    <NuxtLink class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100  hover:text-gray-700 w-full " to="/credit">
+                        <svg viewBox="0 0 1024 1024" fill="currentColor" class="w-5 h-5" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M512 833.6c-13.6 0-24-10.4-24-24v-67.2H384c-13.6 0-24-10.4-24-24s10.4-24 24-24h104v-60.8H384c-13.6 0-24-10.4-24-24s10.4-24 24-24h111.2L384 474.4c-4.8-4.8-7.2-10.4-7.2-16.8s2.4-12.8 7.2-16.8c4.8-4.8 10.4-7.2 16.8-7.2s12.8 2.4 16.8 7.2L512 534.4 605.6 440c4.8-4.8 10.4-7.2 16.8-7.2s12.8 2.4 16.8 7.2c9.6 9.6 9.6 24.8 0 33.6l-110.4 112H640c13.6 0 24 10.4 24 24s-10.4 24-24 24H536v60.8h104c13.6 0 24 10.4 24 24s-10.4 24-24 24H536v67.2c0 12.8-10.4 24-24 24z" fill=""></path><path d="M280 1011.2c-32.8 0-115.2-7.2-176.8-71.2-51.2-53.6-75.2-132-72-234.4 8-210.4 193.6-352 272.8-402.4l12.8-8-9.6-11.2c-20-22.4-53.6-66.4-61.6-117.6-4.8-31.2 2.4-59.2 20-80.8 21.6-28 47.2-33.6 64.8-33.6 19.2 0 40 7.2 60.8 20l8.8 6.4 7.2-9.6c18.4-24.8 52-55.2 103.2-56h1.6c52 0 85.6 30.4 104.8 56l7.2 9.6 9.6-6.4c20.8-13.6 41.6-20 60.8-20 17.6 0 43.2 5.6 64.8 33.6 17.6 22.4 24 50.4 19.2 80.8-5.6 37.6-28 80.8-61.6 117.6l-9.6 11.2 12.8 8c79.2 51.2 264.8 192 272.8 402.4 4 102.4-20.8 181.6-72 234.4-61.6 64-144.8 71.2-176.8 71.2H280z m85.6-688.8c-28.8 16-277.6 159.2-286.4 384.8-3.2 89.6 16.8 156.8 58.4 200 48.8 50.4 116 56 142.4 56h464c26.4 0 93.6-5.6 142.4-56 41.6-43.2 61.6-110.4 58.4-199.2-8-226.4-257.6-369.6-285.6-384.8l-4.8-3.2-5.6 1.6c-2.4 0.8-4.8 1.6-7.2 1.6H384c-2.4 0-4.8-0.8-8-1.6l-5.6-1.6-4.8 2.4z m-36-223.2c-6.4 0-16 1.6-27.2 15.2-9.6 12-12.8 26.4-9.6 44.8 6.4 46.4 48 95.2 72.8 115.2l4.8 4 6.4-1.6c3.2-0.8 4.8-1.6 6.4-1.6h257.6c0.8 0 2.4 0.8 4 0.8l8 2.4 4.8-4c24.8-20 66.4-68.8 72.8-115.2 2.4-18.4-0.8-32.8-9.6-44.8-10.4-13.6-20-15.2-27.2-15.2-23.2 0-51.2 24-60 33.6-4.8 4.8-11.2 8-17.6 8-1.6 0-3.2 0-4.8-0.8-8-1.6-15.2-7.2-17.6-15.2-2.4-7.2-27.2-64-81.6-64h-1.6c-56.8 0.8-80 61.6-80.8 64-3.2 8-9.6 13.6-17.6 15.2-1.6 0-3.2 0.8-4.8 0.8-6.4 0-12.8-2.4-17.6-8-8.8-9.6-36.8-33.6-60-33.6z" fill=""></path></g></svg>
+    
+                        <span class="mx-2 text-sm font-medium">Credit</span>
+                    </NuxtLink>
+    
+                    <NuxtLink class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100  hover:text-gray-700 w-full" to="/history">
+                    <svg viewBox="0 0 24 24" class="w-5 h-5" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 8V12L14.5 14.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M5.60423 5.60423L5.0739 5.0739V5.0739L5.60423 5.60423ZM4.33785 6.87061L3.58786 6.87438C3.58992 7.28564 3.92281 7.61853 4.33408 7.6206L4.33785 6.87061ZM6.87963 7.63339C7.29384 7.63547 7.63131 7.30138 7.63339 6.88717C7.63547 6.47296 7.30138 6.13549 6.88717 6.13341L6.87963 7.63339ZM5.07505 4.32129C5.07296 3.90708 4.7355 3.57298 4.32129 3.57506C3.90708 3.57715 3.57298 3.91462 3.57507 4.32882L5.07505 4.32129ZM3.75 12C3.75 11.5858 3.41421 11.25 3 11.25C2.58579 11.25 2.25 11.5858 2.25 12H3.75ZM16.8755 20.4452C17.2341 20.2378 17.3566 19.779 17.1492 19.4204C16.9418 19.0619 16.483 18.9393 16.1245 19.1468L16.8755 20.4452ZM19.1468 16.1245C18.9393 16.483 19.0619 16.9418 19.4204 17.1492C19.779 17.3566 20.2378 17.2341 20.4452 16.8755L19.1468 16.1245ZM5.14033 5.07126C4.84598 5.36269 4.84361 5.83756 5.13505 6.13191C5.42648 6.42626 5.90134 6.42862 6.19569 6.13719L5.14033 5.07126ZM18.8623 5.13786C15.0421 1.31766 8.86882 1.27898 5.0739 5.0739L6.13456 6.13456C9.33366 2.93545 14.5572 2.95404 17.8017 6.19852L18.8623 5.13786ZM5.0739 5.0739L3.80752 6.34028L4.86818 7.40094L6.13456 6.13456L5.0739 5.0739ZM4.33408 7.6206L6.87963 7.63339L6.88717 6.13341L4.34162 6.12062L4.33408 7.6206ZM5.08784 6.86684L5.07505 4.32129L3.57507 4.32882L3.58786 6.87438L5.08784 6.86684ZM12 3.75C16.5563 3.75 20.25 7.44365 20.25 12H21.75C21.75 6.61522 17.3848 2.25 12 2.25V3.75ZM12 20.25C7.44365 20.25 3.75 16.5563 3.75 12H2.25C2.25 17.3848 6.61522 21.75 12 21.75V20.25ZM16.1245 19.1468C14.9118 19.8483 13.5039 20.25 12 20.25V21.75C13.7747 21.75 15.4407 21.2752 16.8755 20.4452L16.1245 19.1468ZM20.25 12C20.25 13.5039 19.8483 14.9118 19.1468 16.1245L20.4452 16.8755C21.2752 15.4407 21.75 13.7747 21.75 12H20.25ZM6.19569 6.13719C7.68707 4.66059 9.73646 3.75 12 3.75V2.25C9.32542 2.25 6.90113 3.32791 5.14033 5.07126L6.19569 6.13719Z" fill="currentColor"></path> </g></svg>
+                        <span class="mx-2 text-sm font-medium">History</span>
+                    </NuxtLink>
+                </div>
+    
+                <div class="space-y-3 ">
+                    <label class="px-3 text-xs text-gray-500 uppercase">Settings</label> 
+    
+                    <NuxtLink class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100  hover:text-gray-700 w-full" to="/settings">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+    
+                        <span class="mx-2 text-sm font-medium">Video settings</span>
+                    </NuxtLink>
+    
+                    <NuxtLink class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100  hover:text-gray-700 w-full" to="/account">
+                        <svg viewBox="0 0 24 24" class="w-5 h-5" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                        <span class="mx-2 text-sm font-medium">Account</span>
+                    </NuxtLink>
+                </div>
+            </nav>
+        </div>
+    </aside>
+    
+    <div class="ml-64 bg-slate-100 h-full min-h-full pb-20">
+        <NuxtPage />
+    </div>
+
+</div>
+</template>
