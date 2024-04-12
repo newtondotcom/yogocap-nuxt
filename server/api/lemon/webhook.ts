946cc061b78599a18e52e372530ec539b79bf0d6
newtondotcom <asphalt9fr@gmail.com>
2024-04-12 11:35:02 +0200

    verify signature

--- server/api/lemon/webhook.ts
+++ server/api/lemon/webhook.ts
@@ -1,4 +1,18 @@
+import crypto from 'crypto';
+
+const config = useRuntimeConfig()
+
+export const verifySignature = (rawBody: Buffer, signature: string, secret: string): boolean => {
+  const hmac = crypto.createHmac('sha256', secret);
+  const digest = Buffer.from(hmac.update(rawBody).digest('hex'), 'utf8');
+  const receivedSignature = Buffer.from(signature, 'utf8');
+
+  return crypto.timingSafeEqual(digest, receivedSignature);
+};
+
 export default defineEventHandler(async (levent) => {
+  console.log('Received webhook event', levent.context.body)
+    const body = levent.context.body
     const event_name = body.meta.event_name
     const user_id = body.meta.custom_data.user_id
     switch (event_name) {
