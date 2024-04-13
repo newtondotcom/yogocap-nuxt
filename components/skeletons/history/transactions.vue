07cd22f043344fd968f3ce0bc514034f6544e6d7
newtondotcom <asphalt9fr@gmail.com>
2024-04-13 23:38:04 +0200

    past transactions skeletons

---
+++ components/skeletons/history/transactions.vue
@@ -0,0 +1,19 @@
+<template>
+  <tr v-for="i in 3">
+    <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+      <div class="flex flex-row align-middle justify-center">
+        <Skeleton class="h-4 w-[100px]" />
+      </div>
+    </td>
+    <td class="whitespace-nowrap px-4 py-2 text-gray-700">
+      <div class="flex flex-row align-middle justify-center">
+        <Skeleton class="h-4 w-[200px]" />
+      </div>
+    </td>
+    <td class="whitespace-nowrap px-4 py-2 text-gray-700">
+      <div class="flex flex-row align-middle justify-center">
+        <Skeleton class="h-4 w-[60px]" />
+      </div>
+    </td>
+  </tr>
+</template>
