c784d3bd7a582be505022390ca5babc068d04724
newtondotcom <asphalt9fr@gmail.com>
2024-04-13 23:12:02 +0200

    shadcn button

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+function goToNvideo() {
+  navigateTo("/nvideo");
+}
+</script>
+
 <template>
 <div class="mt-8 ml-10">
     <h1 class="text-2xl font-bold text-gray-900 sm:text-4xl ml-12 mt-24">
@@ -9,10 +15,6 @@
       Discover the power of video captioning powered by AI to capture your audience attention. 🚀
     </p>
 
-    <NuxtLink to="/nvideo"
-    class="absolute top-2/3 left-3/4 rounded bg-indigo-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-indigo-500"
-    >
-    Try with a video
-  </NuxtLink>
+    <Button @click="goToNvideo" class="absolute top-2/3 left-3/4">Try with a video</Button>
   </div>
 </template>
