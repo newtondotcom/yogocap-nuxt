1b3725e2c02bf84247e29a4a97baa921bfda2916
newtondotcom <asphalt9fr@gmail.com>
2024-04-20 23:32:41 +0200

    everyhting for toasts

--- app.vue
+++ app.vue
@@ -7,10 +7,12 @@ useSeoMeta({
   ogImage: 'https://img.freepik.com/vecteurs-libre/vecteur-degrade-logo-colore-oiseau_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1712534400&semt=sph',
   twitterCard: 'summary_large_image',
 })
+import Toaster from '@/components/ui/toast/Toaster.vue'
 </script>
 
 
 <template>
+  <Toaster />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
