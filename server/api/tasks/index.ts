2d635be322d306a007e2e5dfd6f02f8915a6ec0e
newtondotcom <asphalt9fr@gmail.com>
2024-04-21 15:26:39 +0200

    api to advertise video task compelted

---
+++ server/api/tasks/index.ts
@@ -0,0 +1,25 @@
+import { setVideoDone } from "~/server/data/videos";
+import { updateTask } from "../../data/tasks";
+import { updateAccountAfterVideoDone } from "~/server/data/account";
+import { sendEmailOnVideoDone } from "~/server/data/mail";
+
+export default defineEventHandler(async (event) => {  
+    // Read the request body
+    const body = await readBody(event);
+    
+    // Extract required data from the request body
+    const task_id = body.task_id;
+    const time_transcription = body.time_transcription;
+    const time_alignment = body.time_alignment;
+    const time_encoding = body.time_encoding;
+    const done_at = body.done_at;
+
+    // Update the task data
+    const video_id = await updateTask(task_id, time_transcription, time_alignment, time_encoding, done_at);
+    // Set the video as done
+    const user_id = await setVideoDone(video_id, task_id);
+    // Update the account data
+    await updateAccountAfterVideoDone(user_id);
+    // Send an email to the user
+    await sendEmailOnVideoDone(user_id,video_id);
+})
