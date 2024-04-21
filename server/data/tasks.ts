37ff098e65d3a23f96b2d31f98577f83ea681d14
newtondotcom <asphalt9fr@gmail.com>
2024-04-21 15:27:41 +0200

    funciton regarding task model

---
+++ server/data/tasks.ts
@@ -0,0 +1,21 @@
+import prisma from "~/server/data/prisma";
+
+export async function updateTask(task_id: any, time_transcription: any, time_alignment: any, time_encoding: any, done_at: any) {
+    try {
+        const updatedTask = await prisma.task.update({
+            where: { id: task_id },
+            data: {
+                time_transcription,
+                time_alignment,
+                time_encoding,
+                done_at
+            },
+            select : {
+                video_id : true
+            }
+        });
+        return updatedTask.video_id;
+    } catch (error : any) {
+        throw new Error(`Error updating task: ${error.message}`);
+    }
+}
