110f3e103b2202dce7817a0ced37f5207e8deea1
newtondotcom <asphalt9fr@gmail.com>
2024-04-24 11:40:52 +0200

    moved tasks api endpoint

--- server/api/dashboard/tasks.ts
+++ server/api/dashboard/tasks.ts
@@ -13,11 +13,12 @@ export default defineEventHandler(async (event) => {
     const time_alignment = body.time_alignment;
     const time_encoding = body.time_encoding;
     const done_at = body.done_at;
+    const thumbnail = body.thumbnail;
 
     // Update the task data
     const video_id = await updateTask(task_id, time_transcription, time_alignment, time_encoding, done_at);
     // Set the video as done
-    const user_id = await setVideoDone(video_id, task_id);
+    const user_id = await setVideoDone(video_id, task_id, thumbnail);
     // Update the account data
     await updateAccountAfterVideoDone(user_id);
     // Send an email to the user
