e2d167627d8828e8d3a84418dd14a1558390438f
newtondotcom <asphalt9fr@gmail.com>
2024-04-24 11:41:29 +0200

    creating a new video

--- server/data/videos.ts
+++ server/data/videos.ts
@@ -1,12 +1,40 @@
 import prisma from "./prisma";
 
-export async function setVideoDone(video_id: any, task_id : string) {
+export async function createVideo(user_id: any, name: string, aligned : boolean, emojis : boolean, music : boolean, silent : boolean, task_id : string, length : number, name_s3 : string, s3name : string) {
+    try {
+        const newVideo = await prisma.video.create({
+            data: {
+                user_id,
+                name,
+                aligned,
+                emojis,
+                music,
+                silent,
+                task_id,
+                length,
+                name_s3,
+                stored : s3name,
+                submitted : new Date()
+            },
+            select : {
+                id : true
+            }
+        });
+        return newVideo.id;
+    } catch (error : any) {
+        throw new Error(`Error creating video: ${error.message}`);
+    }
+}
+
+export async function setVideoDone(video_id: any, task_id : string, thumbnail : string) {
     try {
         // mark the video as done
         const updatedVideo = await prisma.video.update({
             where: { id: video_id },
             data: {
-                task_id
+                task_id,
+                done: true,
+                thumbnail
             },
             select : {
                 user_id : true
