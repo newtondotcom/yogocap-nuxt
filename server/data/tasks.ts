b5b77ada062a76371f731d9f900d8e0ebb9fda6e
newtondotcom <asphalt9fr@gmail.com>
2024-04-24 11:41:09 +0200

    creating a new task

--- server/data/tasks.ts
+++ server/data/tasks.ts
@@ -1,5 +1,25 @@
 import prisma from "~/server/data/prisma";
 
+export async function createTask(video_id: any, aligned : boolean, emojis : boolean, music : boolean, silent : boolean) {
+    try {
+        const newTask = await prisma.task.create({
+            data: {
+                video_id,
+                aligned,
+                emojis,
+                music,
+                silent
+            },
+            select : {
+                id : true
+            }
+        });
+        return newTask.id;
+    } catch (error : any) {
+        throw new Error(`Error creating task: ${error.message}`);
+    }
+}
+
 export async function updateTask(task_id: any, time_transcription: any, time_alignment: any, time_encoding: any, done_at: any) {
     try {
         const updatedTask = await prisma.task.update({
