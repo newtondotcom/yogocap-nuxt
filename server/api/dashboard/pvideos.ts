a571a9acbd013716bfbe18b1287f5dec958c218e
newtondotcom <asphalt9fr@gmail.com>
2024-05-09 22:08:43 +0200

    using server side fct to fetch from db bis

--- server/api/dashboard/pvideos.ts
+++ server/api/dashboard/pvideos.ts
@@ -1,5 +1,7 @@
-import { serverSupabaseUser } from '#supabase/server'
+import { getVideos } from '~/server/data/videos'
 import type { Video } from "~/types/types"
+
+/*
 const videos: Video[] = [
   {
           minia : "https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
@@ -96,8 +98,11 @@ const videos: Video[] = [
               type : "success"
             },
       ]
+      */
 
 export default defineEventHandler(async (event) => {
+  const user_id = event.context.user_id
+  const videos = await getVideos(user_id)
   return {
       pvideos: videos
   }
