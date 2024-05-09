3cd447501781e471e6b0cff421239b0a1c5df6f7
newtondotcom <asphalt9fr@gmail.com>
2024-05-10 00:26:46 +0200

    api endpoint to remove video from s3

--- server/api/s3/remove.ts
+++ server/api/s3/remove.ts
@@ -1,3 +1,9 @@
+import { removeVideo } from "~/server/data/s3";
+
 export default defineEventHandler(async (event) => {
   const user_id = event.context.user_id;
+  const body = await readBody(event);
+  const video_id = body.video_id;
+  await removeVideo(video_id);
+  return "Video removed";
 })
