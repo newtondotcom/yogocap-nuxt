6012bfe29ba45c8928fea62909010b6f71232c79
newtondotcom <asphalt9fr@gmail.com>
2024-05-10 23:33:13 +0200

    acknowledge of possibility

--- server/api/s3/clean.ts
+++ server/api/s3/clean.ts
@@ -1,5 +1,6 @@
 import { cleanAllS3 } from "~/server/data/cleans3"
 
+//COULD BE DONE USING BUILT IN S3 LIFECYCLE POLICY
 export default defineEventHandler(async (event) => {
     await cleanAllS3();
 })
