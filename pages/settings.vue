9ee9cde3c9349108dfe8adc734ef1aa1a0e598d0
newtondotcom <asphalt9fr@gmail.com>
2024-05-10 23:32:13 +0200

    settings page with ability to log out and close account

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,3 +1,59 @@
+<script setup lang="ts">
+import { AlertCircle } from 'lucide-vue-next';
+const supabase = useSupabaseClient()
+
+async function accountClosing() {
+  await $fetch('/api/miscs/user-deletion');
+  logout();
+}
+
+async function logout() {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+        console.error('Error logging out:', error.message)
+    }
+    navigateTo('/')
+}
+</script>
+
 <template>
 <DashboardSubtitle title="Settings" subtitle="Here is your settings page ! 🚀" />
+<div class="p-8">
+<Alert variant="destructive">
+    <AlertCircle class="w-4 h-4" />
+    <AlertTitle>Reminder</AlertTitle>
+    <AlertDescription>
+        The subtitled videos are stored on the server for 2 days. After that, they are automatically deleted.
+    </AlertDescription>
+  </Alert>
+
+</div>
+<div class="flex justify-center">
+  <AlertDialog>
+    <AlertDialogTrigger>
+<Button variant="destructive">
+  Close your account
+</Button>
+</AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your account. However, due to legal reasons, we will keep your data for 5 years.
+           Note that your decision won't be effective until maximum one week after you confirm it.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction @click="accountClosing">Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</div>
+
+<div class="flex justify-center mt-10">
+    <Button @click="logout" variant="secondary">
+    Log out
+  </Button>
+</div>
 </template>
