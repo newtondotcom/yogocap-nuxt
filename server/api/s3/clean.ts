5c42565e22f931641e60967f80e9fdfc5961dd3e
newtondotcom <asphalt9fr@gmail.com>
2024-05-10 23:59:49 +0200

    let minio delete files after 2 days

--- server/api/s3/clean.ts
+++
@@ -1,6 +0,0 @@
-import { cleanAllS3 } from "~/server/data/cleans3"
-
-//COULD BE DONE USING BUILT IN S3 LIFECYCLE POLICY
-export default defineEventHandler(async (event) => {
-    await cleanAllS3();
-})
