8510cea90ec6e71549b23d1966ee6075725efaea
newtondotcom <asphalt9fr@gmail.com>
2024-05-10 12:39:27 +0200

    popup announcing free plan on inscription

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,28 @@
 definePageMeta({
     layout: false,
 })
+import { Toaster } from '@/components/ui/sonner'
+import { toast } from 'vue-sonner'
+
+onMounted(() => {
+    toast('At inscription, you receive a free Starter Plan', {
+            description: 'Enjoy it now ! 🎉',
+            /*
+            action: {
+            label: 'Ok',
+            onClick: () => navigateTo('/auth/login'),
+            },
+            */
+            action: {
+            label: 'Got it',
+            onClick: () => {},
+            },
+        })
+    })
 </script>
 
 <template>
+    <Toaster />
     <LandingHeader />
     <LandingAbout />
     <LandingPricing />
