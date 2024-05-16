203257abfcb69560de46e005980bc119f0f248d3
newtondotcom <asphalt9fr@gmail.com>
2024-05-16 14:33:55 +0200

    send the position to ampq queue

--- server/api/dashboard/postvideo.ts
+++ server/api/dashboard/postvideo.ts
@@ -7,7 +7,7 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     
     // Extract required data from the request body
-    const {name, aligned, emojis, music, silent, length, name_s3, s3name } = body;
+    const {name, aligned, emojis, music, silent, length, name_s3, s3name, position } = body;
 
     // Create a new video
     const video_id = await createVideo(user_id, name, aligned, emojis, music, silent, length, name_s3, s3name);
@@ -19,6 +19,6 @@ export default defineEventHandler(async (event) => {
     await assignTasktoVideo(video_id, task_id);
 
     // Send a msg on the queue
-    const message = JSON.stringify({"s3_name" : s3name, "file_name": name_s3, "emoji": emojis, "lsilence": silent,"music":music, "video_aligned": aligned, "key_db": task_id });
+    const message = JSON.stringify({"s3_name" : s3name, "file_name": name_s3, "emoji": emojis, "lsilence": silent,"music":music, "video_aligned": aligned, "key_db": task_id, "position" : position});
     await sendAMQP(message);
 })
