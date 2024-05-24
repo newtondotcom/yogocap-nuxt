efa76939c8f4f0f8818388d037fe1227212f9091
newtondotcom <asphalt9fr@gmail.com>
2024-05-24 16:34:04 +0200

    seo in template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+useSeoMeta({
+  title: 'Yogocap',
+  ogTitle: 'Yogocap',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://img.freepik.com/vecteurs-libre/vecteur-degrade-logo-colore-oiseau_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1712534400&semt=sph',
+  twitterCard: 'summary_large_image',
+})
 import { SquarePlus, Captions, CreditCard, FileClock, History, Bell, CircleUser, Home, LineChart, Menu, Package, Package2, Search, ShoppingCart, Users } from 'lucide-vue-next';
 const route = useRoute()
 const supabase = useSupabaseClient()
