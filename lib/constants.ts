ad71dee9d1b7121585d1014bade38736fb05a0a4
newtondotcom <asphalt9fr@gmail.com>
2024-06-01 17:07:08 +0200

    feat : constants file

---
+++ lib/constants.ts
@@ -0,0 +1,24 @@
+const constants = {
+    NAME_PLAN_SLOW : 'Starter',
+    SLUG_PLAN_SLOW : 'plan-starter',
+    NB_VIDEOS_SLOW : 60,
+    CAN_EMOJIS_SLOW : false,
+    DURATION_SLOW : 60,
+    PRICE_SLOW : 5,
+
+    NAME_PLAN_MEDIUM : 'Premium',
+    SLUG_PLAN_MEDIUM : 'plan-premium',
+    NB_VIDEOS_MEDIUM : 120,
+    CAN_EMOJIS_MEDIUM : true,
+    DURATION_MEDIUM : 180,
+    PRICE_MEDIUM : 15,
+
+    NAME_PLAN_FAST : 'Business',
+    SLUG_PLAN_FAST : 'plan-business',
+    NB_VIDEOS_FAST : 300,
+    CAN_EMOJIS_FAST : true,
+    DURATION_FAST : 360,
+    PRICE_FAST : 30
+}
+
+export default constants;
