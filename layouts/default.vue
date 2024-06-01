df8c9c1513d5e517c0a88f1941740d38e779258f
newtondotcom <asphalt9fr@gmail.com>
2024-06-01 18:08:19 +0200

    change redirect link

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,7 +30,7 @@ async function logout() {
         <div class="hidden border-r bg-muted/40 md:block">
             <div class="flex h-full max-h-screen flex-col gap-2">
                 <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                    <NuxtLink to="/dashboard" class="flex items-center gap-2 font-semibold">
+                    <NuxtLink to="/" class="flex items-center gap-2 font-semibold">
                         <Captions class="h-6 w-6" />
                         <div class="text-primary font-bold text-xl underline">Yogocap</div>
                     </NuxtLink>
