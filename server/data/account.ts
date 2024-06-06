a2e4a66a86d3442dbc421eaf60c6da88a15d0c1a
newtondotcom <asphalt9fr@gmail.com>
2024-06-06 22:15:57 +0200

    values for on join account creation

--- server/data/account.ts
+++ server/data/account.ts
@@ -97,18 +97,18 @@ export async function setNewUser(user_id: any) {
         await prisma.account.create({
             data: {
                 user_id,
-                videos_remaining: 60,
+                videos_remaining: constants.NB_VIDEOS_JOIN,
                 videos_stored: 0,
                 can_emojis: false,
-                can_music: true,
-                current_duration: 180
+                can_music: false,
+                current_duration: constants.DURATION_JOIN,
             }
         });
         await prisma.buyings.create({
             data: {
                 user_id,
                 date: new Date(),
-                plan: constants.SLUG_PLAN_SLOW,
+                plan: constants.SLUG_PLAN_JOIN,
                 onjoin: true
             }
         });
@@ -148,7 +148,7 @@ export async function setPlanPurchased(user_id: any, plan: string) {
                         increment: constants.NB_VIDEOS_MEDIUM
                     },
                     can_emojis: constants.CAN_EMOJIS_MEDIUM,
-                    current_duration: constants.DURATION_MEDIUM
+                    current_duration: constants.DURATION_MEDIUM,
                 }
             });
         }
