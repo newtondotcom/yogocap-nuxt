c6b6a9f5d0b3bafc588575101fd90fa781905f98
newtondotcom <asphalt9fr@gmail.com>
2024-06-06 22:05:05 +0200

    seo on app and not in layout

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 useSeoMeta({
-  title: 'Yogocap',
-  ogTitle: 'Yogocap',
-  description: 'This is my amazing site, let me tell you all about it.',
-  ogDescription: 'This is my amazing site, let me tell you all about it.',
-  ogImage: 'https://img.freepik.com/vecteurs-libre/vecteur-degrade-logo-colore-oiseau_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1712534400&semt=sph',
-  twitterCard: 'summary_large_image',
+    title: 'Yogocap',
+    ogTitle: 'Yogocap App',
+    description: 'Discover Yogocap, the most cost-effective AI-powered solution for subtitling your social media videos. Ensure your message reaches everyone with our unbeatable prices and high-quality subtitles.',
+    ogDescription: 'Discover Yogocap, the most cost-effective AI-powered solution for subtitling your social media videos. Ensure your message reaches everyone with our unbeatable prices and high-quality subtitles.',
+    ogImage: 'https://app.yogocap.com/og-image.png',
+    twitterCard: 'summary_large_image',
 })
 import Toaster from '@/components/ui/toast/Toaster.vue'
 </script>
