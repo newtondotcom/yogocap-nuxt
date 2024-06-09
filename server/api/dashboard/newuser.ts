1ea2b08d53091e6e9697f159b1010976ad296098
newtondotcom <asphalt9fr@gmail.com>
2024-06-09 17:47:45 +0200

    add email on user account creation

--- server/api/dashboard/newuser.ts
+++ server/api/dashboard/newuser.ts
@@ -1,7 +1,10 @@
 import { setNewUser } from "~/server/data/account";
+import { serverSupabaseUser } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-    const user_id = event.context.user_id
-    const state = await setNewUser(user_id);
+    const user_id = event.context.user_id;
+    const user = await serverSupabaseUser(event);
+    const email = user?.user_metadata.email;
+    const state = await setNewUser(user_id,email);
     return state;
   })
