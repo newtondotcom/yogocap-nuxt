5300a1da7e5846d406907f60746ef112726c867c
newtondotcom <asphalt9fr@gmail.com>
2024-06-11 19:38:10 +0200

    added watermark

--- pages/test.vue
+++ pages/test.vue
@@ -1,3 +1,9 @@
 <template>
-        <div class="text-primary font-bold text-5xl underline px-8 py-8">Y</div>
+        <div class="text-primary font-bold text-7xl underline px-8 py-8 m-[100px]">Y</div>
+
+
+        <div class="p-[100px] bg-black">
+                <div class="text-primary font-bold text-6xl">Made w/</div>
+                <div class="text-primary font-bold text-6xl underline cursor-pointer">Yogocap</div>
+        </div>
 </template>
