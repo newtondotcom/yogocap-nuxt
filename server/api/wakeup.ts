60105ece12629ae7a1151aad5cb9396d8af9022d
newtondotcom <asphalt9fr@gmail.com>
2024-07-27 22:29:42 +0200

    feat : wakeup script for supabase

---
+++ server/api/wakeup.ts
@@ -0,0 +1,25 @@
+import { defineEventHandler } from 'h3';
+import prisma from '../data/prisma';
+
+export default defineEventHandler(async (event) => {
+    const user = await prisma.api.create({
+        data: {
+            key: 'wakeup',
+        },
+        select: {
+            id: true,
+        },
+    });
+    await prisma.api.delete({
+        where: {
+            id: user.id,
+        },
+    });
+
+    return {
+        status: 200,
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+    };
+});
