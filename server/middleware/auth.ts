a93384746d6a2f53c8b8d42061e083678601425a
newtondotcom <asphalt9fr@gmail.com>
2024-08-02 20:15:15 +0200

    fix : auth middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,9 +1,24 @@
-import { serverSupabaseUser } from '#supabase/server'
+import { serverSupabaseUser } from '#supabase/server';
 
 export default defineEventHandler(async (event) => {
-    const endpoint = event.path.split('/').pop()
-    const user = await serverSupabaseUser(event)
-    if (user) {
-        event.context.user_id = user.id
+    const path = event.path.split('/');
+    if (
+        path?.includes('auth') ||
+        path?.includes('wakeup') ||
+        (path[0] == '' && path[1].includes('code'))
+    ) {
+        return;
+    } else {
+        try {
+            const user = await serverSupabaseUser(event);
+            if (user) {
+                event.context.user_id = user.id;
+            }
+        } catch (error) {
+            console.error('Route fetched : ' + event.path);
+            console.error('Error in auth middleware:', error);
+            await sendRedirect(event, '/auth/login', 302);
+            throw new Error('Unauthorized');
+        }
     }
-})
+});
