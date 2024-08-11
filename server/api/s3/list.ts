580f9d0373163e7021fd577d3ee9dfaeaa0992e9
newtondotcom <asphalt9fr@gmail.com>
2024-08-11 16:53:01 -0600

    fix : reject if bearer is missing

--- server/api/s3/list.ts
+++ server/api/s3/list.ts
@@ -4,6 +4,9 @@ export default defineEventHandler(async (event) => {
     const headers = new Headers(event.headers);
     const authorization = headers.get('authorization');
     const first_part = authorization.split(' ')[0] == 'Bearer';
+    if (first_part == false) {
+        await setResponseStatus(event, 401);
+    }
     const apikey = authorization.split(' ')[1];
     const s3s = await lists3(apikey);
     return s3s;
