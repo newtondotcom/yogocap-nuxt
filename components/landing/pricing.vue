3840fa218bc7bd284835d262777d91bf8cf1c6f3
newtondotcom <asphalt9fr@gmail.com>
2024-09-01 16:32:41 +0200

    fix : change plan order

--- components/landing/pricing.vue
+++ components/landing/pricing.vue
@@ -21,26 +21,26 @@
             <div class="divide-y divide-gray-200 rounded-2xl border border-gray-200 shadow-sm">
                 <div class="p-6 sm:px-8">
                     <h2 class="text-lg font-medium text-gray-900">
-                        Business
+                        Starter
                         <span class="sr-only">Plan</span>
+                        <Badge class="ml-1">Our best offer</Badge>
                     </h2>
 
                     <p class="mt-2 text-gray-700">
-                        Full access to
-                        <span class="text-primary">Yogocap</span>
-                        features for profesionnal users, including everything maxed 🔥 and faster
-                        video rendering 🏎️
+                        A plan to begin with
+                        <span class="text-primary">Yogocap 🚀</span>
+                        which includes basic features and low limitations for a tiny budget.
                     </p>
 
                     <p class="mt-2 sm:mt-4">
                         <strong class="text-3xl font-bold text-gray-900 sm:text-4xl">
-                            {{ constants.PRICE_FAST }}€
+                            {{ constants.PRICE_SLOW }}€
                         </strong>
 
                         <span class="text-sm font-medium text-gray-700">one time</span>
                     </p>
 
-                    <Button @click="action(constants.SLUG_PLAN_FAST)" class="mt-4">
+                    <Button @click="action(constants.SLUG_PLAN_SLOW)" class="mt-4">
                         {{ text }}
                     </Button>
                 </div>
@@ -65,7 +65,7 @@
                                 />
                             </svg>
 
-                            <span class="text-gray-700">{{ constants.NB_VIDEOS_FAST }} videos</span>
+                            <span class="text-gray-700">{{ constants.NB_VIDEOS_SLOW }} videos</span>
                         </li>
 
                         <li class="flex items-center gap-1">
@@ -85,7 +85,7 @@
                             </svg>
 
                             <span class="text-gray-700">
-                                {{ constants.DURATION_FAST / 60 }} min maximum per video
+                                {{ constants.DURATION_SLOW / 60 }} min maximum per video
                             </span>
                         </li>
 
@@ -142,12 +142,12 @@
                                 viewBox="0 0 24 24"
                                 stroke-width="1.5"
                                 stroke="currentColor"
-                                class="h-5 w-5 text-primary"
+                                class="h-5 w-5 text-red-700"
                             >
                                 <path
                                     stroke-linecap="round"
                                     stroke-linejoin="round"
-                                    d="M4.5 12.75l6 6 9-13.5"
+                                    d="M6 18L18 6M6 6l12 12"
                                 />
                             </svg>
 
@@ -161,12 +161,12 @@
                                 viewBox="0 0 24 24"
                                 stroke-width="1.5"
                                 stroke="currentColor"
-                                class="h-5 w-5 text-primary"
+                                class="h-5 w-5 text-red-700"
                             >
                                 <path
                                     stroke-linecap="round"
                                     stroke-linejoin="round"
-                                    d="M4.5 12.75l6 6 9-13.5"
+                                    d="M6 18L18 6M6 6l12 12"
                                 />
                             </svg>
 
@@ -336,26 +336,26 @@
             <div class="divide-y divide-gray-200 rounded-2xl border border-gray-200 shadow-sm">
                 <div class="p-6 sm:px-8">
                     <h2 class="text-lg font-medium text-gray-900">
-                        Starter
+                        Business
                         <span class="sr-only">Plan</span>
-                        <Badge class="ml-1">Our best offer</Badge>
                     </h2>
 
                     <p class="mt-2 text-gray-700">
-                        A plan to begin with
-                        <span class="text-primary">Yogocap 🚀</span>
-                        which includes basic features and low limitations for a tiny budget.
+                        Full access to
+                        <span class="text-primary">Yogocap</span>
+                        features for profesionnal users, including everything maxed 🔥 and faster
+                        video rendering 🏎️
                     </p>
 
                     <p class="mt-2 sm:mt-4">
                         <strong class="text-3xl font-bold text-gray-900 sm:text-4xl">
-                            {{ constants.PRICE_SLOW }}€
+                            {{ constants.PRICE_FAST }}€
                         </strong>
 
                         <span class="text-sm font-medium text-gray-700">one time</span>
                     </p>
 
-                    <Button @click="action(constants.SLUG_PLAN_SLOW)" class="mt-4">
+                    <Button @click="action(constants.SLUG_PLAN_FAST)" class="mt-4">
                         {{ text }}
                     </Button>
                 </div>
@@ -380,7 +380,7 @@
                                 />
                             </svg>
 
-                            <span class="text-gray-700">{{ constants.NB_VIDEOS_SLOW }} videos</span>
+                            <span class="text-gray-700">{{ constants.NB_VIDEOS_FAST }} videos</span>
                         </li>
 
                         <li class="flex items-center gap-1">
@@ -400,7 +400,7 @@
                             </svg>
 
                             <span class="text-gray-700">
-                                {{ constants.DURATION_SLOW / 60 }} min maximum per video
+                                {{ constants.DURATION_FAST / 60 }} min maximum per video
                             </span>
                         </li>
 
@@ -457,12 +457,12 @@
                                 viewBox="0 0 24 24"
                                 stroke-width="1.5"
                                 stroke="currentColor"
-                                class="h-5 w-5 text-red-700"
+                                class="h-5 w-5 text-primary"
                             >
                                 <path
                                     stroke-linecap="round"
                                     stroke-linejoin="round"
-                                    d="M6 18L18 6M6 6l12 12"
+                                    d="M4.5 12.75l6 6 9-13.5"
                                 />
                             </svg>
 
@@ -476,12 +476,12 @@
                                 viewBox="0 0 24 24"
                                 stroke-width="1.5"
                                 stroke="currentColor"
-                                class="h-5 w-5 text-red-700"
+                                class="h-5 w-5 text-primary"
                             >
                                 <path
                                     stroke-linecap="round"
                                     stroke-linejoin="round"
-                                    d="M6 18L18 6M6 6l12 12"
+                                    d="M4.5 12.75l6 6 9-13.5"
                                 />
                             </svg>
 
