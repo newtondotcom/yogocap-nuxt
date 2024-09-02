ca3c51a1ffbc7ac9d4c113f37e65ca7249c4c3c5
newtondotcom <asphalt9fr@gmail.com>
2024-09-02 12:17:59 +0200

    fix : ui for landing page for mobile

--- pages/index.vue
+++ pages/index.vue
@@ -17,16 +17,18 @@
 
 <template>
     <div class="mt-8 ml-10">
-        <h1 class="text-2xl font-bold text-gray-900 sm:text-4xl ml-6 mt-24">
+        <h1 class="text-2xl font-bold text-gray-900 sm:text-4xl mx-auto md:ml-6 mt-24">
             Welcome to
             <span class="">Yogocap</span>
         </h1>
 
-        <p class="mt-1.5 text-l text-gray-600">
+        <p class="mt-1.5 text-gray-600">
             Discover the power of video captioning powered by AI to capture your audience attention.
             🚀
         </p>
 
-        <Button @click="goToNvideo" class="absolute top-2/3 left-3/4">Try with a video</Button>
+        <Button @click="goToNvideo" class="absolute top-2/3 left-1/2 md:left-3/4">
+            Try with a video
+        </Button>
     </div>
 </template>
